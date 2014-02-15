5646d3a6d5f52a0c316d2e4eb65581d570ea1f92
James Bucanek <james@gloaming.com>
2014-02-14 22:38:41 -0700

    Added credits to the settings view controller.

---
+++ Pigeon/CreditsViewController.h
@@ -0,0 +1,16 @@
+//
+//  CreditsViewController.h
+//  Pigeon
+//
+//  Created by James Bucanek on 2/14/14.
+//  Copyright (c) 2014 Dawn to Dusk Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreditsViewController : UIViewController
+
+- (IBAction)linkToBook:(id)sender;
+- (IBAction)linkToGitHub:(id)sender;
+
+@end
---
+++ Pigeon/CreditsViewController.m
@@ -0,0 +1,55 @@
+//
+//  CreditsViewController.m
+//  Pigeon
+//
+//  Created by James Bucanek on 2/14/14.
+//  Copyright (c) 2014 Dawn to Dusk Software. All rights reserved.
+//
+
+#import "CreditsViewController.h"
+
+
+static void OpenAnURL( NSString *url );
+
+//@interface CreditsViewController ()
+//@end
+
+@implementation CreditsViewController
+
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    if (self) {
+//        // Custom initialization
+//    }
+//    return self;
+//}
+
+//- (void)viewDidLoad
+//{
+//    [super viewDidLoad];
+//	// Do any additional setup after loading the view.
+//}
+
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+
+- (IBAction)linkToBook:(id)sender
+{
+	OpenAnURL(@"http://x.co/iosappdev");
+}
+
+- (IBAction)linkToGitHub:(id)sender
+{
+	OpenAnURL(@"http://x.co/gitpigeon");
+}
+
+@end
+
+static void OpenAnURL( NSString *url )
+{
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
+}
